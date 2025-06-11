0ffc24b0c039f4d5f01f4b570f88de9ec0c7598b
Alankar Singh <alankar010499@gmail.com>
2025-06-11 20:21:46 +0530

    corrected the code v0.1.1

--- parse.go
+++ parse.go
@@ -7,15 +7,15 @@ import (
 	"golang.org/x/net/html"
 )
 
-// Links represent a Link  (<a href=".."> ) from the html deocument
-
+// Link represents a link (<a href="...">) in an HTML
+// document.
 type Link struct {
-	href string
-	text string
+	Href string
+	Text string
 }
 
-// html.Parse return html nodes
-// Parsing is done by calling Parse with an io.Reader, which returns the root of the parse tree (the document element) as a *Node. It is the caller's responsibility to ensure that the Reader provides UTF-8 encoded HTML. For example, to process each anchor node in depth-first order:
+// Parse will take in an HTML document and will return a
+// slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
 	doc, err := html.Parse(r)
 	if err != nil {
@@ -24,45 +24,44 @@ func Parse(r io.Reader) ([]Link, error) {
 	nodes := linkNodes(doc)
 	var links []Link
 	for _, node := range nodes {
-		links = append(links, BuildLink(node))
-
+		links = append(links, buildLink(node))
 	}
-
 	return links, nil
 }
 
-func BuildLink(n *html.Node) Link {
-	var res Link
+func buildLink(n *html.Node) Link {
+	var ret Link
 	for _, attr := range n.Attr {
 		if attr.Key == "href" {
-			res.href = attr.Val
+			ret.Href = attr.Val
 			break
 		}
 	}
-	res.text = text(n)
-	return res
+	ret.Text = text(n)
+	return ret
 }
 
 func text(n *html.Node) string {
-	var res string
 	if n.Type == html.TextNode {
-		res = n.Data
+		return n.Data
 	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		res += text(c) + " "
+		ret += text(c)
 	}
-	return strings.Join(strings.Fields(res), " ")
+	return strings.Join(strings.Fields(ret), " ")
 }
 
 func linkNodes(n *html.Node) []*html.Node {
-	var res []*html.Node
 	if n.Type == html.ElementNode && n.Data == "a" {
 		return []*html.Node{n}
 	}
-
+	var ret []*html.Node
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		res = append(res, linkNodes(c)...)
-
+		ret = append(ret, linkNodes(c)...)
 	}
-	return res
+	return ret
 }
